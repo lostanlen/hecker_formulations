6426e39d96b830cfe7d4bd0e0d87186b101165ce
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2017-12-10 18:06:18 -0500

    update generate_hecker_formulations_sbatch.py

--- generate_hecker_formulations_sbatch.py
+++ generate_hecker_formulations_sbatch.py
@@ -20,7 +20,7 @@ for file_id in range(1, 1+n_files):
         f.write("#SBATCH --nodes=1\n")
         f.write("#SBATCH --tasks-per-node=1\n")
         f.write("#SBATCH --cpus-per-task=1\n")
-        f.write("#SBATCH --time=0:10:00\n")
+        f.write("#SBATCH --time=1:00:00\n")
         f.write("#SBATCH --mem=32GB\n")
         f.write("#SBATCH --output=../slurm/slurm_" + job_name + "_%j.out\n")
         f.write("\n")
